e1fda1c9d6ea52d99e41f3a62837f7a7af691ef5
matto <matto@damniam.de>
2017-07-06 10:50:13 +0200

    added session management and configuration

--- oi_link.py
+++ oi_link.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
+from configparser import ConfigParser
+from os.path import realpath, dirname, join
+from functools import wraps
 from random import choices
 from string import ascii_letters
+from sys import exit
 
-
-from flask import Flask, request, redirect, render_template_string
+from flask import Flask, request, redirect, render_template_string, session, url_for
 
 
 OI_LINK = Flask(__name__)
 
 oi_links = {}
+
+# -----------------------------------------------------
+# read config 
+# -----------------------------------------------------
+
+
+try:
+    oi_link_config = ConfigParser()
+    oi_link_path = dirname(realpath(__file__))
+    oi_link_config_path = join(oi_link_path, 'oi_link.conf')
+    oi_link_config.read(oi_link_config_path)
+    username = oi_link_config['OI_LINK']['user']
+    password = oi_link_config['OI_LINK']['password']
+    oi_link_port = int(oi_link_config['OI_LINK']['port'])
+    OI_LINK.secret_key = oi_link_config['OI_LINK']['SECRET']
+except:
+    print('please check configfile: ', oi_link_config_path)
+    exit(1)
+
+
+# -----------------------------------------------------
+# oi_link functions
+# -----------------------------------------------------
 
 def generate_link():
     global oi_links
@@ -20,7 +46,51 @@ def generate_link():
     return link
 
 
+def oi_link_session(wrapped):
+    @wraps(wrapped)
+    def oi_link_request(*args, **kwargs):
+        if 'username' in session:
+            return wrapped(*args, **kwargs)
+        else:
+            return 'You are not logged in <br><a href="' + url_for('login') + '">login</a>'
+    return oi_link_request
+
+
+# -----------------------------------------------------
+# flask routes
+# -----------------------------------------------------
+
+@OI_LINK.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == username and request.form['password'] == password:
+            session.permanent = True
+            session['username'] = username
+        return redirect(url_for('home'))
+    return '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                <form action="" method="post">
+                    <p><input type=text name=username>
+                    <p><input type=password name=password>
+                    <p><input type=submit value=Login>
+                </form>
+            </body>
+        </html>
+    '''
+
+
+@OI_LINK.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
 @OI_LINK.route('/', methods = ['POST', 'GET'])
+@oi_link_session
 def home():
     if request.method == 'POST': 
         oi_submit = dict(request.form)
@@ -47,9 +117,10 @@ def home():
                 <form action="" method="post" id="oi_form">
                     <p><input type=submit value=LINK></p>
                     <p><input type=radio name=oi_link_type value=paste_link checked>PASTE LINK</p>
-                    <p><input type=radio name=oi_link_type value=short_link checked>SHORT LINK</p>
+                    <p><input type=radio name=oi_link_type value=short_link>SHORT LINK</p>
                     <textarea style="width: 100%;" name="oi_link_content" form="oi_form">text or link here</textarea>
                 </form>
+                <a href="/logout">logout</a>
             </body>
         </html>
     '''
@@ -92,4 +163,4 @@ def unpack_oi_link(oi_link):
 
 
 if __name__ == '__main__':
-    OI_LINK.run(debug=True, port=8000)
+    OI_LINK.run(debug=True, port=oi_link_port)
